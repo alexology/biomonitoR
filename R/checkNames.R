1b0d04f3d89a91fff5ece7eb8a2bae1ffa4b9f29
alexology <alex.laini@gmail.com>
2017-04-13 16:41:32 +0200

    Various updates

--- R/checkNames.R
+++ R/checkNames.R
@@ -4,7 +4,7 @@ checkNames <- function(x){
 
   # nameCheck and nameSuggest check for the wrong names and suggest for correct names.
   # hunspell_check and hunspell_suggest are from the package hunspell
-  nameCheck <- hunspell_check(taxaCar, dict = dictio)
-  nameSuggest <- hunspell_suggest(taxaCar, dict = dictio)
+  nameCheck <- hunspell::hunspell_check(taxaCar, dict = dictio)
+  nameSuggest <- hunspell::hunspell_suggest(taxaCar, dict = dictio)
   sum(nameCheck)==length(nameCheck)
 }
