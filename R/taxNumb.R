ee265a11c33caa93a25b59dd1a56c54ea1f86a24
alexology <alex.laini@gmail.com>
2017-09-22 16:36:05 +0200

    Function taxNumb added

---
+++ R/taxNumb.R
@@ -0,0 +1,19 @@
+taxNumb <- function(x){
+
+  # check if the object d is of class "biomonitoR"
+
+  if (class(x) != "biomonitoR") {
+    opt <- options(show.error.messages = FALSE)
+    on.exit(options(opt))
+    return("Object x is not an object of class biomonitoR")
+  }
+
+
+  tax <- x[["Taxa"]]
+  if("unassigned" %in% fam[,1]){
+    z <- which(tax$Taxa=="unassigned")
+    tax <- tax[-z,] # remove unassigned row from the species count
+  }
+  tax <- apply(tax[,-1], 2, FUN=function(x){length(x[x>0])})
+  return(tax)
+}
