82838d1677e7eafc63e3af4b06c77f445cc88259
alexology <alex.laini@gmail.com>
2017-11-20 15:35:27 +0100

    changes to combTaxa

--- R/combTaxa.R
+++ R/combTaxa.R
@@ -1,6 +1,6 @@
 #' combTaxa
 #'
-#' This function calculates the absolute or relative abundance of a Taxon or of a set Taxa. 
+#' This function calculates the absolute or relative abundance of a Taxon or of a set Taxa.
 #' @param x results of function aggregatoR.
 #' @param taxa a Taxon or a vector of taxa.
 #' @param rel if TRUE calculates relative abundance. default =F.
@@ -17,30 +17,30 @@
 
 
 combTaxa <- function(x, ntaxa = 2, taxLev = "Family", rel = F){
-  
+
   # check if the object d is of class "biomonitoR"
-  
+
   if (class(x) != "biomonitoR") {
     opt <- options(show.error.messages = FALSE)
     on.exit(options(opt))
     return("Object x is not an object of class biomonitoR")
   }
-  
+
   df <- x[[taxLev]]
-  
+
   if("unassigned" %in% df[,1]){
     z <- which(df[,1]=="unassigned")
     df <- df[-z,] # remove unassigned row from the species count
   }
-  
+
   cbn <- combn(nrow(df), ntaca)
   df.sub <- lapply(seq(ncol(cbn)), function(x) df[cbn[,x],])
-  
-  if(rel = T){
+
+  if(rel == T){
     df.agg <- lapply(df.sub, FUN = agg_fun)
   } else {
     df.agg <- lapply(df.sub, FUN = agg_fun)
   }
-    
+
   return(do.call(rbind, df.agg))
 }
