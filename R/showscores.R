96abf6b94e6eb891f4403c4f4d7c999609d506d8
alexology <alex.laini@gmail.com>
2017-12-04 15:58:52 +0100

    function showscores added

---
+++ R/showscores.R
@@ -0,0 +1,26 @@
+#' showscores
+#'
+#' This function print the scores used for the calculation of several indices in biomonitoR.
+#' @param x name of the scores to print. Allowed names are "aspt_b", "aspt_i", "aspt_h", "life_scores_spe" and "life_scores_fam"
+#' @param writecsv if TRUE the scores are saved in the working directory.
+#' @keywords asBiomonitor
+#' @export
+#' @seealso \code{\link{aspt}}, \code{\link{bmwp}}, \code{\link{life}}
+#' @examples
+#' showscores("aspt_i", writecsv = F)
+
+showscores <- function(x, writecsv = F){
+  # list of allowed scores
+  score.list <- c("aspt_b", "aspt_i", "aspt_h", "life_scores_spe", "life_scores_fam")
+  if(!x %in% score.list){
+    stop("provide a valid name")
+  }
+  else {
+    if(writecsv = F){
+      return(x)
+    } else {
+      write.csv(x, paste(x, ".csv", sep =""))
+      return(x)
+    }
+  }
+}
