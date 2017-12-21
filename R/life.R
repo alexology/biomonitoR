34e2ea151bf6aa5825a5291c645de1821d52ee10
alexology <alex.laini@gmail.com>
2017-12-21 17:18:48 +0100

    changes to life function

--- R/life.R
+++ R/life.R
@@ -2,7 +2,9 @@
 #'
 #' This function calculates LIFE index according to Extence et al. (1999).
 #' @param x results of function aggregatoR function
-#' @param method possible choices
+#' @param taxLev possible choices are "Family" and "Species"
+#' @param method possible methods are "original" and "revised_2010"
+#' @param abucl abundance threshold. Default 0, 9, 99, 999, 9999
 #' @keywords life
 #' @details life currently calculates life index to family and species level, following the nomenclature used by Extence et al. (1999). Species level nomenclature of Extence et al. (1999) is outdated, an updated version will be released soon in biomonitoR.
 #' @references Extence CA, Balbi DM, Chadd RP. 1999. River flow indexing using British benthic macroinvertebrates: a framework for setting hydroecological objectives. Regulated Rivers: Research and Management 15: 543–574.
@@ -14,7 +16,7 @@
 #' data.agR <- aggregatoR(data.bio)
 #' data.life <- life(data.agR, method = "Family")
 
-life <- function(x, method = "Family"){
+life <- function(x, taxLev = "Family", method = "original", abucl = c(0,9,99,999,9999)){
 
   if (class(x) != "biomonitoR") {
     opt <- options(show.error.messages = FALSE)
@@ -22,13 +24,21 @@ life <- function(x, method = "Family"){
     return("Object x is not an object of class biomonitoR")
   }
 
-  if(method == "Family"){
+  if(taxLev == "Family" & method == "original"){
     life_scores <- life_scores_fam
+    fs_life <- fs_life
   }
 
-  if(method == "Species"){
-    life_scores <- life_scores_spe
+  if(taxLev == "Family" & method == "revised_2010"){
+    life_scores <- life_scores_fam_2010
+    fs_life <- fs_life_2010
   }
+
+  if(taxLev == "Species" & method == "original"){
+    life_scores <- life_scores_spe
+    fs_life <- fs_life
+  }
+
 
   numb <- c(which(names(x)=="Tree"), which(names(x)=="Taxa")) # position of the Tree element in the list to remove
   fam <- x[-numb, drop = F]
@@ -53,14 +63,20 @@ life <- function(x, method = "Family"){
   # keep only numeric columns
   temp <- fam.long[, 3, drop = F]
 
+  A <- abucl[1]
+  B <- abucl[2]
+  C <- abucl[3]
+  D <- abucl[4]
+  E <- abucl[5]
+
   # transform row abundances to abunance classes
   names(temp) <- "ABU_NUM"
-  temp[temp==0] <- 0
-  temp[temp>=1&temp<=9] <- 1
-  temp[temp>=10&temp<=99] <- 2
-  temp[temp>=100&temp<=999] <- 3
-  temp[temp>=1000&temp<=9999] <- 4
-  temp[temp>=10000] <- 5
+  temp[temp==A] <- 0
+  temp[temp>=c(A+1) &temp<=B] <- 1
+  temp[temp>=c(B+1)&temp<=C] <- 2
+  temp[temp>=c(C+1)&temp<=D] <- 3
+  temp[temp>=c(D+1)&temp<=E] <- 4
+  temp[temp>=c(E+1)] <- 5
 
   fam.long <- data.frame(fam.long, temp)
   fam.long <- merge(fam.long, life_scores)
