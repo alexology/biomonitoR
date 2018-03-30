e79eb04fde61b62359e67f96c4e18b601db6d86f
alexology <alex.laini@gmail.com>
2018-03-30 16:21:53 +0200

    changes to life and whpt functions

--- R/whpt.R
+++ R/whpt.R
@@ -3,9 +3,10 @@
 #' This function calculates WHPT index according to most recent version used in UK.
 #' @param x results of aggregatoR function
 #' @param taxLev currently only the option "Family" is enabled
-#' @param type possible choices are "aspt", "ntaxa", "bmwp"
+#' @param type presence only ("po") or abundance ("ab")
+#' @param metric possible choices are "aspt", "ntaxa", "bmwp"
 #' @param composite if T composite families as listed in the details section are used
-#' @param abucl abundance threshold. Default 0, 9, 99, 999, 9999.
+#' @param abucl abundance threshold. Default 0, 9, 99, 999.
 #' @keywords whpt
 #' @details WHPT is a revision of BMWP and it takes into account the abundances of organisms. The following aggregation is used if composit is set equal to T:
 #'
@@ -29,7 +30,7 @@
 #' data.agR <- aggregatoR(data.bio)
 #' data.whpt <- whpt(data.agR, taxLev = "Family", composite = F)
 
-whpt <- function(x, taxLev = "Family", type = "aspt", composite = F, abucl = c(0,9,99,999,9999)){
+whpt <- function(x, taxLev = "Family", type = "ab", metric = "aspt", composite = F, abucl = c(0,9,99,999)){
   
   if (class(x) != "biomonitoR") {
     opt <- options(show.error.messages = FALSE)
@@ -37,8 +38,12 @@ whpt <- function(x, taxLev = "Family", type = "aspt", composite = F, abucl = c(0
     return("Object x is not an object of class biomonitoR")
   }
   
-  if(type != "aspt" & type != "ntaxa" & type != "bmwp"){
-    stop("Please provide a valide type: aspt, ntaxa or bmwp")
+  if(type != "po" & type != "ab"){
+    stop("Please provide a valide type: po or ab")
+  }
+  
+  if(metric != "aspt" & metric != "ntaxa" & metric != "bmwp"){
+    stop("Please provide a valide metric: aspt, ntaxa or bmwp")
   }
   
   if(taxLev != "Family"){
@@ -75,28 +80,44 @@ whpt <- function(x, taxLev = "Family", type = "aspt", composite = F, abucl = c(0
                       idvar="Taxon", times = names(fam)[-1], timevar = "Site")
   rownames(fam.long) <- NULL
   
-  # keep only numeric columns
-  temp <- fam.long[, 3, drop = F]
-  
-  A <- abucl[1]
-  B <- abucl[2]
-  C <- abucl[3]
-  D <- abucl[4]
-  E <- abucl[5]
-  
-  # transform row abundances to abunance classes
-  names(temp) <- "ABU_NUM"
-  temp[temp==A] <- 0
-  temp[temp>=c(A+1) &temp<=B] <- 1
-  temp[temp>=c(B+1)&temp<=C] <- 2
-  temp[temp>=c(C+1)&temp<=D] <- 3
-  temp[temp>=c(D+1)&temp<=E] <- 4
-  temp[temp>=c(E+1)] <- 5
-  
-  fam.long <- data.frame(fam.long, temp)
-  fam.long <- merge(fam.long, whpt_scores_use)
 
-  fam.sub <- fam.long[,c(3,5)]
+  
+  # presence only
+  
+  if(type = "po"){
+    whpt_scores_use <- whpt_scores_use[ which( whpt_scores_use$ABU_NUM == 1) , ] # keep the po scores only
+    fam.long <- fam.long[ which( fam.long$Abu > 0) , ]
+    fam.long <- merge(fam.long, whpt_scores_use)
+  }
+  
+  if(type = "ab"){
+    
+    whpt_scores_use <- whpt_scores_use[ which( whpt_scores_use$ABU_NUM != 1) , ] # exclude the po scores
+    
+    # keep only numeric columns
+    temp <- fam.long[, 3, drop = F]
+  
+    A <- abucl[1]
+    B <- abucl[2]
+    C <- abucl[3]
+    D <- abucl[4]
+
+  
+    # transform row abundances to abunance classes
+    names(temp) <- "ABU_NUM"
+    temp[temp==A] <- 0
+    temp[temp>=c(A+1) & temp<=B] <- 2
+    temp[temp>=c(B+1) & temp<=C] <- 3
+    temp[temp>=c(C+1) & temp<=D] <- 4
+    temp[temp>=c(D+1)] <- 5
+
+  
+    fam.long <- data.frame(fam.long, temp)
+    fam.long <- merge(fam.long, whpt_scores_use)
+  }
+  
+  columns <- c("Site", "Score") # columns to retain for calculations
+  fam.sub <- fam.long[ , columns]
   fam.whpt <- aggregate(. ~ Site, fam.sub, FUN = sum)
   fam.whpt$rich <- aggregate(. ~ Site, fam.sub, FUN = length)[,2]
   fam.whpt$score <- fam.whpt[,2] / fam.whpt[,3]
