2fa019bc5431961c10c8eab39cb3b7bd3be38b90
alexology <alex.laini@gmail.com>
2018-07-20 17:03:46 +0200

    upload checked functions

--- R/dness.R
+++ R/dness.R
@@ -1,7 +1,7 @@
 #' dness
 #'
 #' This function calculates the taxonomic diversity, taxonomic distinctness and variation of taxonomic distinctness.
-#' @param x results of function aggregatoR
+#' @param d results of function aggregatoR
 #' @param complete if TRUE unassigned taxon are removed from the taxonomic list of the desired taxonomic level set with taxLev
 #' @param taxLev taxonomic level from which the indices are to be calculated
 #' @param method available methods are delta (taxonomic diversity), delta.st (taxonomic distinctness) and delta.bin (variation in taxonomic distinctness)
@@ -16,69 +16,66 @@
 #' data(macro_ex)
 #' data.bio <- asBiomonitor(macro_ex)
 #' data.agR <- aggregatoR(data.bio)
-#' eptd(data.agR)
+#' dness(data.agR, complete = TRUE)
 
 
 
 
-dness <- function(d, complete = F, taxLev = "Genus", method = "delta", taxatree = F) {
+dness <- function(d, complete = FALSE, taxLev = "Genus", method = "delta", taxatree = FALSE) {
   # check if the object d is of class "biomonitoR"
 
-  if (class(d) != "biomonitoR") {
-    opt <- options(show.error.messages = FALSE)
-    on.exit(options(opt))
-    return("Object x is not an object of class biomonitoR")
-  }
+  # check if the object x is of class "biomonitoR"
+  classCheck(x)
 
   # check if there are unassigned taxon in the taxonomic tree
   # if present stop the function, unless the user specify unassigned = T
 
 
   df <- d[[taxLev]]
-  st.names <- names(df[, -which(names(df) %in% c(taxLev)), drop = F])   # sites name
+  st.names <- names(df[, -which(names(df) %in% c(taxLev)), drop = FALSE])   # sites name
   tax <- df[ , taxLev]    # taxa names at the desired taxonomic level taxLev
   che.ck <- "unassigned" %in% tax   # check if unassigned is present at the desired taxonomic level
   tree.c <- d[["Tree"]]
   # exclude taxonomic levels with missing information. This feature could be improved in the next future
   exclude <- -which(names(tree.c) %in% c("Taxa", "Subclass", "Subfamily", "Tribus", "Subspecies"))
-  tree <- tree.c[ , exclude, drop = F]
+  tree <- tree.c[ , exclude, drop = FALSE]
   # taxonomic levels to retain
   tax.pos <- names(tree[, 1:which(names(tree) == taxLev)])
   ref.c <- tree.c[ , c(which(names(tree.c) %in%  c( tax.pos, "Taxa" ) ))]
 
-  if(complete == F & che.ck == T) {stop("Unassigned taxon at the desired taxonomic level")}
-  if(complete == F & che.ck == F){
+  if(complete == FALSE & che.ck == TRUE) {stop("Unassigned taxon at the desired taxonomic level")}
+  if(complete == FALSE & che.ck == FALSE){
     # retain only the taxonomic levels from order to taxLev and sites
 
     colnames(df)[which( names(df) == taxLev)] <- "Taxa"
-    taxtree <- merge(ref.c, df, by = "Taxa", all = F)
-    taxtree <- taxtree[, which(names(taxtree) %in% c(tax.pos, st.names)), drop = F]
+    taxtree <- merge(ref.c, df, by = "Taxa", all = FALSE)
+    taxtree <- taxtree[, which(names(taxtree) %in% c(tax.pos, st.names)), drop = FALSE]
   }
 
-  if(complete == T & che.ck == T){
+  if(complete == TRUE & che.ck == TRUE){
     colnames(df)[which( names(df) == taxLev)] <- "Taxa"
     df <- df[-which(df$Taxa == "unassigned"),]
-    taxtree <- merge(ref.c, df, by = "Taxa", all = F)
-    taxtree <- taxtree[, which(names(taxtree) %in% c(tax.pos, st.names)), drop = F]
+    taxtree <- merge(ref.c, df, by = "Taxa", all = FALSE)
+    taxtree <- taxtree[, which(names(taxtree) %in% c(tax.pos, st.names)), drop = FALSE]
   }
 
   # remove empty columns (maybe is not a necessary step)
-  temp <- taxtree[ , which(names(taxtree) %in% c(tax.pos)), drop = F]
+  temp <- taxtree[ , which(names(taxtree) %in% c(tax.pos)), drop = FALSE]
   temptf <- temp != ""
   # remove columns with at least 1 empty cell
   check.col <- apply(temptf, 2, sum) / nrow(temptf)
   if(sum(check.col) != length(temp)){
-    taxtree <- taxtree[, - which(check.col != 1), drop = F]
+    taxtree <- taxtree[, - which(check.col != 1), drop = FALSE]
   }
 
-  if(ncol(taxtree[, -which(names(taxtree) %in% c(st.names)), drop = F]) == 1){
+  if(ncol(taxtree[, -which(names(taxtree) %in% c(st.names)), drop = FALSE]) == 1){
     stop("Reference database has not enough taxonomic levels to perform the analysis")
   }
 
 
   # calculating taxonomic distance
   tax.dis <- ddis(taxtree, st.names = st.names)
-  sites <- taxtree[ , which(names(taxtree) %in% st.names), drop = F]
+  sites <- taxtree[ , which(names(taxtree) %in% st.names), drop = FALSE]
   sites.bin <- sites
   sites.bin[sites.bin > 0 ] <- 1
 
@@ -86,6 +83,6 @@ dness <- function(d, complete = F, taxLev = "Genus", method = "delta", taxatree 
   if(method == "delta.bin") {res <- apply(sites.bin, 2, function( x ) ( delta(x, dis = tax.dis)))}
   if(method == "delta.st") {res <- apply(sites, 2, function( x ) ( delta.st(x, dis = tax.dis)))}
 
-  if(taxatree == F) return(res)
-  if(taxatree == T) return(list(res, taxtree))
+  if(taxatree == FALSE) return(res)
+  if(taxatree == TRUE) return(list(res, taxtree))
 }
