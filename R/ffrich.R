3db497db216a0d7077ef5b81cb7a812369a5816d
alexology <alex.laini@gmail.com>
2018-08-10 15:57:20 +0200

    changes to csi, cwm and ffrich functions

--- R/ffrich.R
+++ R/ffrich.R
@@ -1,4 +1,4 @@
-#' Fuzzy coded functional richness 
+#' Fuzzy coded functional richness
 #'
 #' This function calculates the functional richness based on trait category.
 #'
@@ -59,7 +59,7 @@
 #' @export
 
 ffrich <- function(x, traitDB = NULL, agg = FALSE, traitSel = FALSE, taxLev = "Family", traceB = FALSE, nbdim = 7, metric = "Gower", corr_method = FALSE){
-  
+
   # check if user provided a trait database, otherwise use traitsTachet
   # if traitsTachet has to be used check for class biomonitoR and "mi"
   if( is.null( traitDB )){
@@ -76,31 +76,31 @@ ffrich <- function(x, traitDB = NULL, agg = FALSE, traitSel = FALSE, taxLev = "F
     traitDB[ , "Taxa"] <- as.factor( sapply( trim( traitDB[ , "Taxa"] ), capWords, USE.NAMES = F ) )
     mes <- FALSE
   }
-  
+
   if( traitSel == TRUE){
     rma <- select.list( names( traitDB[ -which( names( traitDB ) %in% "Taxa")] ) , title = "Traits selection"  , graphics = TRUE , multiple = T )
-    traitDB <- traitDB %>% 
+    traitDB <- traitDB %>%
       select( c("Taxa", rma) )
     # trim and capitalise the column Taxa of the user' trait database
     traitDB$Taxa <- apply( as.data.frame( trim( traitDB$Taxa ) ) , 1 , capWords)
   }
-  
-  
+
+
   abundances <- x[[taxLev]]
   colnames(abundances)[1] <- "Taxa"
   taxa <- as.character(abundances$Taxa)
-  
-  
+
+
   if( isTRUE(mes) | agg == TRUE ){
     # create dummy variables to avoid R CMD check NOTES
     traitsTachet <- Taxa <- modality <- affinity <- Phylum <- Subspecies <-
       Abundance <- Sample <- Weight <- Affinity <- totWeight <-
       weightedAffinity <- Category <- . <- NULL
-    
+
     if (! taxLev %in% c("Family", "Genus", "Species", "Taxa")) {
       return("taxLev should be one of the following: Family, Genus, Species or Taxa")
     }
-    
+
     # I included also Ad., Gen. and Lv. in gsub pattern. Currently biomonitoR does not handle adults and larvae at the same time
     trait_db <- traitDB                               %>%
       (function(df) {
@@ -115,12 +115,12 @@ ffrich <- function(x, traitDB = NULL, agg = FALSE, traitSel = FALSE, taxLev = "F
       spread(key = modality, value = affinity)        %>%
       ungroup()
       trait_db$Taxa <- trimws(trait_db$Taxa)
-    
+
 
     if (length(taxa[taxa != "unassigned"]) == 0) {
       return("At least one taxa should be identified at a level compatible with the indicated taxLev")
     }
-    
+
     if (taxLev == "Taxa") {
       level <- sapply(select(x$Tree, Phylum:Subspecies),
                       function(i) {
@@ -132,8 +132,10 @@ ffrich <- function(x, traitDB = NULL, agg = FALSE, traitSel = FALSE, taxLev = "F
     } else {
       level <- rep(taxLev, length(taxa))
     }
-    
-    taxa_traits <- mutate(mi_ref, Taxa = as.character(Taxa)) %>%
+
+    ref <- select(x$Tree, Phylum:Taxa)
+
+    taxa_traits <- mutate(ref, Taxa = as.character(Taxa)) %>%
       left_join(mutate(trait_db, Taxa = as.character(Taxa)),
                 by = "Taxa")                              %>%
       (function(df) {
@@ -146,13 +148,13 @@ ffrich <- function(x, traitDB = NULL, agg = FALSE, traitSel = FALSE, taxLev = "F
           do.call(what = rbind) %>%
           data.frame(Taxa = taxa, ., stringsAsFactors = FALSE)
       })
-    
+
   }
-  
+
   if( mes == FALSE & agg == FALSE ){
     taxa_traits <- traitDB
   }
-    
+
   taxa_traits <- as.data.frame(taxa_traits)
   # be sure that taxa_traits contains only the Taxa present in the user's community data
   taxa_traits <- taxa_traits[ taxa_traits[, "Taxa"]  %in% abundances[, "Taxa"] , ]
@@ -163,16 +165,16 @@ ffrich <- function(x, traitDB = NULL, agg = FALSE, traitSel = FALSE, taxLev = "F
   taxa_traits <- taxa_traits[complete.cases(taxa_traits), ]
   # remove categories with sum = 0
   taxa_traits <- taxa_traits[ , colSums(taxa_traits) > 0 , drop = F ]
-  # remove rows also in abundances 
+  # remove rows also in abundances
   abundances <- abundances[ rownames(taxa_traits) , ]
 
-  
+
   qual_fs <- qfs(taxa_traits , nbdim = nbdim, metric = metric, corr_method = corr_method)
   m <- qual_fs$meanSD<0.01
-  
+
   if(!any( m == TRUE)) { stop("there is no optimal number of dimension, please check your data for possible problems")}
-  m <- min( which( m == TRUE ) ) + 1 
-  
+  m <- min( which( m == TRUE ) ) + 1
+
   fric <- fric_3d( t(abundances[, -which( names( abundances ) %in% "Taxa")]), fpc = qual_fs$fpc, m = m)
   fric[ which( is.na( fric ) ) ]<-0
   if( traceB == F ){
