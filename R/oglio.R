20c5e43a04121c83d81ebcad006c47e39e2bf13d
alexology <alex.laini@gmail.com>
2018-10-24 15:36:06 +0200

    macrophyte dataset added

---
+++ R/oglio.R
@@ -0,0 +1,7 @@
+#' Macrophytes example dataset
+#'
+#'
+#' This data were collected in the Oglio river, northern ITaly. It consist of cover abundances on a scale from 0 to 100.
+#'
+#' @format A data frame with 67 rows and 7 variables:
+"oglio"
