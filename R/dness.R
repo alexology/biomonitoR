c05c54881bde743a298b20b6ae2c05fa507893c2
alexology <alex.laini@gmail.com>
2018-10-27 17:13:39 +0200

    documentation updated

--- R/dness.R
+++ R/dness.R
@@ -14,9 +14,9 @@
 #' @export
 #' @seealso \code{\link{aggregatoR}}
 #' @examples
-#' data(macro_ex)
-#' data.bio <- asBiomonitor(macro_ex)
-#' data.agR <- aggregatoR(data.bio)
+#' data(oglio)
+#' data.bio <- asBiomonitor(oglio)
+#' data.agR <- aggregatoR(oglio.bio)
 #' dness(data.agR, complete = TRUE)
 
 
@@ -31,12 +31,12 @@ dness <- function(x, complete = FALSE, taxLev = "Genus", method = "delta", taxat
   # if present stop the function, unless the user specify unassigned = T
 
   df <- x[[taxLev]]
-  
+
   # check if the selected taxonomic level contains a least one taxa
   if( nrow( df ) == 1 & df[ 1 , 1 ] == "unassigned" ){ stop("The selected taxonomic level is empty") }
   if( taxLev == "Taxa"){ stop("taxLev cannot be equal to Taxa") }
-  
-  
+
+
   st.names <- names(df[, -which(names(df) %in% c(taxLev)), drop = FALSE])   # sites name
   tax <- df[ , taxLev]    # taxa names at the desired taxonomic level taxLev
   che.ck <- "unassigned" %in% tax   # check if unassigned is present at the desired taxonomic level
@@ -50,7 +50,7 @@ dness <- function(x, complete = FALSE, taxLev = "Genus", method = "delta", taxat
   ref.c <- tree.c[ , c(which(names(tree.c) %in%  tax.pos ))]
   # remove duplicated rows
   ref.c <- ref.c[ ! duplicated( ref.c ) ,  ]
-  
+
   if(complete == FALSE & che.ck == TRUE) {stop("Unassigned taxon at the desired taxonomic level")}
   if(complete == FALSE & che.ck == FALSE){
     # retain only the taxonomic levels from order to taxLev and sites
@@ -63,7 +63,7 @@ dness <- function(x, complete = FALSE, taxLev = "Genus", method = "delta", taxat
     taxtree <- merge(ref.c, df, by = taxLev, all = FALSE)
     taxtree <- taxtree[, c( tax.pos , st.names ), drop = FALSE]
   }
-  
+
   if(complete == TRUE & che.ck == FALSE){
     taxtree <- merge(ref.c, df, by = taxLev, all = FALSE)
     taxtree <- taxtree[, c( tax.pos , st.names ), drop = FALSE]
@@ -77,15 +77,15 @@ dness <- function(x, complete = FALSE, taxLev = "Genus", method = "delta", taxat
   if(sum(check.col) != length(temp)){
     taxtree <- taxtree[, - which(check.col != 1), drop = FALSE]
   }
-  
+
   # remove columns distinct for all species or with the same taxa name for all the rows (excluding taxLev)
   # -1 is to eclude taxLev from this computation
   tax.pos2 <- 1:(which(names(taxtree) == taxLev) - 1)
   tax.uniq <- apply( taxtree[ , tax.pos2 , drop = FALSE] , 2 , function(x) ( length(unique(x) ) ) )
   col.rm <- -which(tax.uniq == 1 | tax.uniq == nrow(taxtree) )
   if(length(col.rm) != 0) (taxtree <- taxtree[ , col.rm , drop = FALSE ])
-  
-  
+
+
   if(ncol(taxtree[, -which(names(taxtree) %in% c(st.names)), drop = FALSE]) == 1){
     stop("Reference database has not enough taxonomic levels to perform the analysis")
   }
