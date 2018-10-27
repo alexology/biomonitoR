4d38b1ebf0adb13d51f4e740456dfaa847910a4c
alexology <alex.laini@gmail.com>
2018-10-27 16:23:37 +0200

    documentation updated

--- R/oglio.R
+++ R/oglio.R
@@ -1,7 +1,11 @@
 #' Macrophytes example dataset
 #'
+#' These data were collected in the Oglio river, northern Italy. This data.frame consists of cover abundances on a scale from 0 to 100.
 #'
-#' This data were collected in the Oglio river, northern ITaly. It consist of cover abundances on a scale from 0 to 100.
+#' @usage data(grav)
 #'
-#' @format A data frame with 67 rows and 7 variables:
+#' @keywords datasets
+#'
+#' @format A data frame with 67 rows and 7 variables.
+#'
 "oglio"
