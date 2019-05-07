41afa5749ae5f114d558d2c7e36efca010f2d938
alexology <alex.laini@gmail.com>
2019-05-07 17:05:58 +0200

    traitScaling help updated

--- R/traitScaling.R
+++ R/traitScaling.R
@@ -34,16 +34,6 @@
 #' @param filter_by_distance filter the results according to the taxonomic distance. Possible values are "pos" , "neg" or a positive integer. See details.
 #' @note USE WITH CAUTION, STILL IN DEVELOPMENT.
 #'
-#' @return a data frame with the following columns:
-#' \enumerate{
-#'  \item **results**: results of the ffred function;
-#'  \item **traits**: a data.frame containing the traits used for the calculations;
-#'  \item **taxa**: a data.frame conaining the taxa used for th calculations;
-#'  \item **nbdim**: number of dimensions used after calculatin the quality of functional spaces according to Maire et al., (2015);
-#'  \item **taxa_not_used**: a vector conaining the names of the taxa exluded from the calculations. Taxa are excluded from the calculations if they have NA values at least in one of the trait modalities;
-#'  \item **problematic traits**: traits containing NAs for *taxa_not_used*
-#'  \item **traits_not_used**: trait modalities excluded from the calculations because they have 0 value for each species.
-#' }
 #'
 #' @importFrom dplyr '%>%' mutate select left_join group_by summarise ungroup
 #' @importFrom tidyr gather spread
