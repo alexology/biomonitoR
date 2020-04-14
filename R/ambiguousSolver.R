254faa82a2a9eefa1c7b6f553f37149bd09a5a5c
Alex <alex.laini@gmail.com>
2020-04-15 00:34:45 +0200

    bug fixed, now the tree is ordered prior to the analysis. Order is from rows with ubspecies to row with only Phylum. This implementation gives consistent results independently of the order of names in the Tree data.frame.

--- R/ambiguousSolver.R
+++ R/ambiguousSolver.R
@@ -2,10 +2,14 @@
 #'
 #' Solve the ambiguous assignment of taxa.
 #'
-#' @param x result of function aggregatoR.
-#' @param method only method *RPKC* and *MCWP* is currently implemented. See details.
-#' @details Taxonomic dataset often contains ambiguous taxa due to the difficulites in identify damaged and juvenile organisms. For instance Chironominae and Chironomidae could reported as in the same sample. Several techinques has been
-#' proposed to solve this issue (see Cuffney et al., 2007). Currently ambiguousSolver has only one algorithm that allow to assing the abundances of lowr taxonomic level to those of higher taxonomic level.
+#' @param x result of the function aggregatoR.
+#' @param method only methods `RPKC` and `MCWP` are currently implemented in `biomonitoR`. See details.
+#' @details Taxonomic dataset often contains ambiguous taxa due to the difficulites in identify damaged and juvenile organisms.
+#' For instance Chironominae and Chironomidae could be reported in the same sample. Several techinques has been
+#' proposed to solve this issue (see Cuffney et al., 2007).
+#' Currently ambiguousSolver two only one algorithms to solve the problem of ambiguous assignments.
+#' `RPKC` means Remove Parents - Keep Children. It works by removing all the higher taxonomic levels while keeping children.
+#' `MCWP` mean Merge Children with Parents. It works by assignign the abundances of lower taxonomic levels to those of higher taxonomic levels.
 #' @keywords aggregatoR
 #' @references Cuffney, T. F., Bilger, M. D., & Haigler, A. M. (2007). Ambiguous taxa: effects on the characterization and interpretation of invertebrate assemblages. Journal of the North American Benthological Society, 26(2), 286-307.
 #' @export
@@ -14,17 +18,25 @@
 #' data( macro_ex )
 #' data.bio <- asBiomonitor( macro_ex )
 #' data.agR <- aggregatoR( data.bio )
-#' df <- ambiguousSolver( data.agR )
-#' df.bio <- asBiomonitor( df )
-#' df.agR <- aggregatoR( df.bio )
+#' data_amb_solved <- ambiguousSolver( data.agR )
+#' data_amb_solved.bio <- asBiomonitor( data_amb_solved )
+#' data_amb_solved.agR <- aggregatoR( data_amb_solved.bio )
 
 
 ambiguousSolver <- function( x , method = "MCWP" ){
 
+  # check if the object d is of class "biomonitoR"
   classCheck( x )
 
+  # Extract the taxonomic tree
   tree <- x[[ "Tree" ]]
 
+  # order tree from the rows containing species to the rows containing only Phylum ( if present, obviously )
+  # order also alphabetically ( maybe not needed )
+  n.tl <- apply( tree[ , 1:10 ] , 1 , function( x ) max( which( x != "" ) ) )
+  tree <- tree[ order( n.tl , decreasing = TRUE ) , ]
+
+  # Extract the taxa list as charcater and get the position of the column called taxa in the taxonomic tree
   taxa <- as.character( tree$Taxa )
   taxa.pos <- which( names( tree ) == "Taxa" )
 
@@ -32,7 +44,13 @@ ambiguousSolver <- function( x , method = "MCWP" ){
   # this should speed up the computation time
   taxa.acc <- c()
 
+  # RPKC: basically, for each taxon in the taxonomic list we extract the taxonomic tree from taxlev -1 to Phylum
+  # taxlev -1 represents the taxonomic level before the taxonomic level of the working taxon: temp <- temp[ -length( temp ) ]
+  # if any of the taxa along the tree from taxlev -1 until Phylum is present in the column taxa we have a problem of
+  # ambiguous taxa and we store the results in the taxa.acc vector. The higher taxonomic levels are then deleted from the database.
+
   if( method == "RPKC" ){
+
     for( i in 1:length( taxa ) ){
       temp <- tree[  i , 1:(taxa.pos - 1) ]
       temp <- temp[ temp != "" ]
@@ -42,16 +60,27 @@ ambiguousSolver <- function( x , method = "MCWP" ){
       }
     }
 
+    # get unique higher taxa names
     taxa.acc <- unique( taxa.acc )
-    taxa.pc <- rep( TRUE, length( taxa ) )
-    taxa.pc[ taxa %in% taxa.acc ] <- FALSE
 
-    df <- x[["Taxa"]][ taxa.pc , ]
-    names( df )[ 1 ] <- "Taxa"
+    # make a vector of the same length as the column taxa, set value to TRUE
+    # and set ambiguous taxa to FALSE. The orignal taxa list is used
+    # instead of the ordered
+    taxa.pc <- rep( TRUE, length( taxa ) )
+    taxa.pc[ x[[ "Taxa" ]][ , "Taxon" ] %in% taxa.acc ] <- FALSE
+
+    # parents are thus removed from the database
+    DF <- x[[ "Taxa" ]][ taxa.pc , ]
+    names( DF )[ 1 ] <- "Taxa"
   }
 
-  if( method == "MCWP"){
-    for( i in 1:length( taxa ) ){taxa
+  # MCWP: basically, the taxonomic tree of each taxon is retrieved. The presence of ambiguos taxa in the taxonomic tree is
+  # checked against the Taxa column of the user database. If present we have ambiguous taxa to solve, otherwise no.
+  # The taxon name of the i-th row is set to the highest taxonomic level if ambiguous parents are present, otherwise no.
+
+
+  if( method == "MCWP" ){
+    for( i in 1:length( taxa ) ){
       if( any( taxa[ i ] %in% taxa.acc ) ){
         next
       } else {
@@ -72,10 +101,13 @@ ambiguousSolver <- function( x , method = "MCWP" ){
       }
     }
 
-    df <-  data.frame( Taxa = as.factor( taxa ) , tree[ , ( taxa.pos + 1 ):ncol( tree ) , drop = FALSE ] )
+    # build the data.frame and order it as the original
+    DF <-  data.frame( Taxa = as.factor( taxa ) , tree[ , ( taxa.pos + 1 ):ncol( tree ) , drop = FALSE ] )
+    DF <- DF[ match( 1:nrow( tree ) , as.numeric( rownames( DF ) ) ) , ]
 
   }
 
-  df <- aggregate( . ~ Taxa , data = df , FUN = sum )
-  df
+  # aggregate to avoid duplicated rownames
+  DF <- aggregate( . ~ Taxa , data = DF , FUN = sum )
+  DF
 }
