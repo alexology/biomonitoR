5b249e5e125f1d718507b2a4364c36aa70ea1383
Alex <alex.laini@gmail.com>
2020-06-10 15:34:48 +0200

    added check.names to FALSE to avoid problems when column names are numbers

--- R/to_bin.R
+++ R/to_bin.R
@@ -4,7 +4,7 @@ to_bin <- function( x ){
   x_num <- unlist( lapply( x , is.numeric ) )
   x_bin <- x[ , x_num ]
   x_bin[ x_bin > 0 ] <- 1
-  x.df <- data.frame( x[ , ! x_num , drop = FALSE ] , x_bin )
+  x.df <- data.frame( x[ , ! x_num , drop = FALSE ] , x_bin , check.names = FALSE )
   class( x.df[ , ! x_num  ] ) <- class( x[ , ! x_num   ] )
   x.df
 }
